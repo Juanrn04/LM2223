9f0c155a2e47fc5f4220feba75cf138f6e4a3347
juanrn04 <juanrodriguez04@iesamachado.org>
2023-05-25 13:44:57 +0200

    Añadido

--- 3Trimestre/Repaso_examen/Modelo_examen1/Ej2.js
+++ 3Trimestre/Repaso_examen/Modelo_examen1/Ej2.js
@@ -11,17 +11,19 @@
     })
 })*/
 
-$(document).ready(function (){
-    $("#btn-producto").click(function(){
+$(document).ready(function () {
+    $("#btn-producto").click(function () {
         let libro = $("#codProducto").val();
 
         $.ajax({
-            url:"https://fakestoreapi.com/products/"+libro,
-            succes: function(data){
+            url: "https://fakestoreapi.com/products/"+libro,
+            success: function (data) {
                 console.log(data);
                 $("#resultados").html(`
-                <h1>Titulo: ${data.title} </h1>
-                <h2>Precio: ${data.price}</h2>
+                <p>${data.title} </p>
+                <img height="150px" src="${data.image}" >
+                <p>Precio: ${data.price}€<p>
+                <p>${data.description}</p>
                 `);
             },
             fail: function(){
@@ -32,4 +34,26 @@ $(document).ready(function (){
             },
         });
     });
+
+    $("btn-todos").click(function(){
+        $.get("https://fakestoreapi.com/products/").done(function (data) {
+            console.log(data);
+            let num_productos = data.length;
+            $("#resultados").empty();
+            for (let i = 0; i < num_productos; i++) {
+                let titulo = data[i].title;
+                let precio = data[i].price;
+                let imagen = data[i].image;
+                let descripcion = data[i].description;                
+                $('#resultados').html(
+                    '<p>' + '<img width="120" src="' + imagen + '"></p>' +
+                    '<p>Título: ' + titulo + '</p>' +
+                    '<p>Descripción: ' + descripcion + '</p>' +
+                    '<p>Precio: ' + precio + '€</p>');
+            }
+        }).fail( function (){
+            $("#resultados").html("Error cargando datos del servidor");
+            }
+        );
+    })
 });
