ccbf019695eabc71ab4a07532bf8a56d6058538e
juanrn04 <juanrodriguez04@iesamachado.org>
2023-05-25 21:25:10 +0200

    Añadido

--- 3Trimestre/Repaso_examen/Modelo_examen2/Ej2.js
+++ 3Trimestre/Repaso_examen/Modelo_examen2/Ej2.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+
+
+    $("#btn-todos").click(function (e) {
+        e.preventDefault();
+        $.ajax({
+                url:"https://gutendex.com/books/",
+                success: function (data) {
+                    console.log(data);
+                    let num_productos = data.results.length;
+                    $("#resultados").empty();
+                    for (let i = 0; i < num_productos; i++) {
+                    $("#resultados").append(`
+                    <p>${data.results[i].title}</p>
+                    <p>Autor: + ${data.results[i].authors.name} +</p> 
+                    <img src="${data.results[i].formats['image/jpeg']}">
+                    <p><a href="${data.results[i].formats["text/plain"]}" target="_blanck">Descargar aqui</a></p>
+                    <p>Num de descargas: ${data.results[i].download_count}</p>
+                    `);
+                    }
+                },
+                fail: function()
+                {
+                    $("#resultados").html("<p>No se encontró el libro.</p>");
+                },
+                error: function()
+                {
+                    $("#resultados").html("<p>Error al buscar el libro</p>");
+                },
+            });
+        });
+
+
+
     $("#btn-libro").click(function(){
         let codlibro = $("#codLibro").val();
         $.ajax({
@@ -22,33 +55,6 @@ $(document).ready(function () {
 });
 
 
-$("#btn-todos").click(function (e) {
-    e.preventDefault();
-    $.ajax({
-            url:"https://gutendex.com/books/",
-            success: function (data) {
-                console.log(data);
-                let num_productos = data.length;
-                $("#resultados").empty();
-                for (let i = 0; i < num_productos; i++) {
-                $("#resultados").append(`
-                <p >${data[i].title} </p>
-                <p>Autor: ${data[i].authors.name}</p> 
-                <img src="${data[i].formats['image/jpeg']}">
-                <p><a href="${data[i].formats["text/plain"]}" target="_blanck">Descargar aqui</a></p>
-                <p>Num de descargas: ${data[i].download_count}</p>
-                `);
-                }
-            },
-            fail: function()
-            {
-                $("#resultados").html("<p>No se encontró el libro.</p>");
-            },
-            error: function()
-            {
-                $("#resultados").html("<p>Error al buscar el libro</p>");
-            },
-        });
-    });
+
 
 });
